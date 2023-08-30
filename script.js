690dc6ec3ba5c2a8817d20070a5a7399c7a58e72
Peter Babo <33118245+babopeter@users.noreply.github.com>
2023-08-30 15:02:54 +0200

    Create objects directly in the array

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-const myLibrary = [book1, book2, book3];
+const myLibrary = [
+    new Book("Assassin's Apprentice", "Robin Hobb", 392, "read"),
+    new Book("The Eye of the World", "Robert Jordan", 685, "read"),
+    new Book("The Way of Kings", "Brandon Sanderson", 1007, "unread")
+];
 
 function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
 }
 
 function addBookToLibrary() {
-  
+    // https://stackoverflow.com/questions/46800576/how-to-create-an-object-with-user-input-javascript
 }
 
-const book1 = new Book("Assassin's Apprentice", "Robin Hobb", 
-392, "read")
+function displayBook() {
 
-const book2 = new Book("The Eye of the World", "Rober Jordan",
-685, "read")
 
-const book3 = new Book("The Way of Kings", "Brandon Sanderson", 
-1007, "unread")
+
+    // const booksUl = document.getElementById('books');
+    // for (let i = 0; i < myLibrary.length; i++) {
+    //     let newLiEl = document.createElement("li");
+    //     booksUl.appendChild(newLiEl);
+    // };
+
+}
+
+displayBook();
