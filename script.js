8ef9a03e90aa7e1e76cf8650ebdd24e684180fec
Peter Babo <33118245+babopeter@users.noreply.github.com>
2023-08-30 14:19:00 +0200

    Add initial files

---
+++ script.js
@@ -0,0 +1,10 @@
+const myLibrary = ["Assassin's Apprentice", "Royal Assassin",
+"Assassin's Quest"];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
