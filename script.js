7c2d440df1142d1e4eff87df8d0da42793d3c5bf
Peter Babo <33118245+babopeter@users.noreply.github.com>
2023-08-30 14:42:16 +0200

    Create object consturctor and dummy objects

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const myLibrary = ["Assassin's Apprentice", "Royal Assassin",
-"Assassin's Quest"];
+const myLibrary = [book1, book2, book3];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  
 }
+
+const book1 = new Book("Assassin's Apprentice", "Robin Hobb", 
+392, "read")
+
+const book2 = new Book("The Eye of the World", "Rober Jordan",
+685, "read")
+
+const book3 = new Book("The Way of Kings", "Brandon Sanderson", 
+1007, "unread")
